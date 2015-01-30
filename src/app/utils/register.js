20e0c80047a986658cbfd63d2867d54c7cb10ab3
Michael Bromley <michael@michaelbromley.co.uk>
2015-01-30 10:50:27 +0000

    Fix for bug when directives use templateUrl
    See https://github.com/michaelbromley/angular-es6/issues/1

--- src/app/utils/register.js
+++ src/app/utils/register.js
@@ -107,7 +107,12 @@ function register(appName) {
         // The factoryArray uses Angular's array notation whereby each element of the array is the name of a
         // dependency, and the final item is the factory function itself.
         factoryArray.push((...args) => {
-            return new constructorFn(...args);
+            //return new constructorFn(...args);
+            var instance = new constructorFn(...args);
+            for (var key in instance) {
+                instance[key] = instance[key];
+            }
+            return instance;
         });
 
         return factoryArray;
